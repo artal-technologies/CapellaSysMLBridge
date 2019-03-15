4c80c317bb497ddfe34ee373141805f859b8217a
BINOT Yann <yann.binot@artal.fr>
2019-03-15 10:55:39 +0100

    comment

--- Plugins/com.artal.capella.mapping.sysml2capella/src/com/artal/capella/mapping/sysml2capella/rules/FunctionalExchangeMapping.java
+++ Plugins/com.artal.capella.mapping.sysml2capella/src/com/artal/capella/mapping/sysml2capella/rules/FunctionalExchangeMapping.java
@@ -79,9 +79,61 @@ public class FunctionalExchangeMapping extends AbstractMapping {
 		transformObjectFlow(eResource, _source, logicalFunctionRoot, functionRule.getMapPinToParam(), true);
 	}
 
+	/**
+	 * Transform {@link ObjectFlow} to {@link FunctionalExchange}. Browse all
+	 * the {@link Activity} breakdown to transform all {@link ObjectFlow}
+	 * 
+	 * @param eResource
+	 *            the Sysml resource
+	 * @param activity
+	 *            the {@link Activity} containing the {@link ObjectFlow}
+	 * @param functionContainer
+	 *            the LogicalFunction parent where is added the
+	 *            {@link FunctionalExchange}
+	 * @param mapPinToParam
+	 *            {@link Map} allowing to have the link between the {@link Pin}
+	 *            and the {@link ActivityParameterNode}
+	 * @param firstLevel
+	 *            at the first level the {@link ActivityParameterNode} are
+	 *            managed.
+	 */
 	private void transformObjectFlow(Resource eResource, Activity activity, LogicalFunction functionContainer,
 			Map<Pin, ActivityParameterNode> mapPinToParam, boolean firstLevel) {
+
+		transformEdges(eResource, activity, functionContainer, mapPinToParam, firstLevel);
+
+		AbstractMapping rule = MappingRulesManager.getRule(LogicalFunctionMapping.class.getName());
 		EList<ActivityNode> nodes = activity.getNodes();
+		for (ActivityNode node : nodes) {
+			if (node instanceof CallBehaviorAction) {
+				Behavior behavior = ((CallBehaviorAction) node).getBehavior();
+				if (behavior != null) {
+					LogicalFunction lf = (LogicalFunction) rule.getMapSourceToTarget().get(node);
+					transformObjectFlow(eResource, (Activity) behavior, lf, mapPinToParam, false);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Transform {@link ObjectFlow} to {@link FunctionalExchange}.
+	 * 
+	 * @param eResource
+	 *            the Sysml resource
+	 * @param activity
+	 *            the {@link Activity} containing the {@link ObjectFlow}
+	 * @param functionContainer
+	 *            the LogicalFunction parent where is added the
+	 *            {@link FunctionalExchange}
+	 * @param mapPinToParam
+	 *            {@link Map} allowing to have the link between the {@link Pin}
+	 *            and the {@link ActivityParameterNode}
+	 * @param firstLevel
+	 *            at the first level the {@link ActivityParameterNode} are
+	 *            managed.
+	 */
+	private void transformEdges(Resource eResource, Activity activity, LogicalFunction functionContainer,
+			Map<Pin, ActivityParameterNode> mapPinToParam, boolean firstLevel) {
 		EList<ActivityEdge> edges = activity.getEdges();
 		for (ActivityEdge edge : edges) {
 			if (edge instanceof ObjectFlow) {
@@ -122,98 +174,161 @@ public class FunctionalExchangeMapping extends AbstractMapping {
 
 			}
 		}
-		AbstractMapping rule = MappingRulesManager.getRule(LogicalFunctionMapping.class.getName());
-		for (ActivityNode node : nodes) {
-			if (node instanceof CallBehaviorAction) {
-				Behavior behavior = ((CallBehaviorAction) node).getBehavior();
-				if (behavior != null) {
-					LogicalFunction lf = (LogicalFunction) rule.getMapSourceToTarget().get(node);
-					transformObjectFlow(eResource, (Activity) behavior, lf, mapPinToParam, false);
-				}
-			}
-		}
 	}
 
+	/**
+	 * Get capella {@link FunctionPort}.
+	 * 
+	 * @param eResource
+	 *            the Sysml resource
+	 * @param source
+	 *            the {@link Activity} node to treat
+	 * @param mapPinToParam
+	 *            {@link Map} allowing to have the link between the {@link Pin}
+	 *            and the {@link ActivityParameterNode}
+	 * @param isSource
+	 *            true if the port is source
+	 * @param transformParam
+	 * @param objecFlow
+	 *            the source exchange
+	 * @return {@link FunctionPort}
+	 */
 	private FunctionPort getCapellaFunctionPort(Resource eResource, ActivityNode source,
 			Map<Pin, ActivityParameterNode> mapPinToParam, boolean isSource, boolean transformParam,
 			ObjectFlow objecFlow) {
 
 		if (source instanceof Pin) {
-			ActivityParameterNode parameter = mapPinToParam.get(source);
-			if (parameter != null) {
-				return getCapellaFunctionPort(eResource, parameter, mapPinToParam, source instanceof InputPin, true,
-						objecFlow);
-			}
-			EObject sourcePortParent = source.eContainer();
-			AbstractMapping rule = MappingRulesManager.getRule(LogicalFunctionPortMapping.class.getName()
-					+ Sysml2CapellaUtils.getSysMLID(eResource, sourcePortParent));
-			if (rule == null) {
-				return null;
-			}
-			return (FunctionPort) rule.getMapSourceToTarget().get(source);
+			return manageExchangeWithPin(eResource, source, mapPinToParam, objecFlow);
 		} else if (source instanceof ActivityParameterNode) {
-			EObject sourcePortParent = source.eContainer();
-			if (sourcePortParent instanceof Activity
-					&& ((Activity) sourcePortParent).getName().equals("02 Functional Architecture")) {
-				AbstractMapping rule = MappingRulesManager.getRule(FunctionalArchitectureMapping.class.getName());
-				return (FunctionPort) rule.getMapSourceToTarget().get(source);
-			}
-			if (transformParam) {
-				ActivityParameterNode an = (ActivityParameterNode) source;
-				List<ActivityEdge> e = new ArrayList<ActivityEdge>();
-				e.addAll(an.getIncomings());
-				e.addAll(an.getOutgoings());
-				for (ActivityEdge activityEdge : e) {
-					if (activityEdge instanceof ObjectFlow) {
-						ActivityNode s = activityEdge.getSource();
-						ActivityNode t = activityEdge.getTarget();
-						boolean inDir = an.getParameter().getDirection() == ParameterDirectionKind.IN_LITERAL;
-						if (s.equals(an)) {
-
-							return getCapellaFunctionPort(eResource, t, mapPinToParam, inDir, true,
-									(ObjectFlow) activityEdge);
-						}
-						if (t.equals(an)) {
-							return getCapellaFunctionPort(eResource, s, mapPinToParam, inDir, true,
-									(ObjectFlow) activityEdge);
-						}
-					}
-				}
-			}
+			return manageExchangeWithParam(eResource, source, mapPinToParam, transformParam);
 		}
 		// the association is not linked at a port.
 		else if (source instanceof CallBehaviorAction) {
-			Object object = MappingRulesManager.getRule(LogicalFunctionMapping.class.getName()).getMapSourceToTarget()
-					.get(source);
-			LogicalFunction lfParent = null;
-			if (object instanceof LogicalFunction) {
-				lfParent = (LogicalFunction) object;
-			} else if (object instanceof List) {
-				List<?> list = (List<?>) object;
-				for (Object object2 : list) {
-					if (object2 instanceof LogicalFunction) {
-						lfParent = (LogicalFunction) object2;
-						break;
-					}
-				}
-			}
-
-			FunctionPort port = null;
-			if (isSource) {
-				port = FaFactory.eINSTANCE.createFunctionInputPort();
-				lfParent.getInputs().add((FunctionInputPort) port);
-
-			} else {
-				port = FaFactory.eINSTANCE.createFunctionOutputPort();
-				lfParent.getOutputs().add((FunctionOutputPort) port);
-			}
-			port.setName(objecFlow.getName());
-			Sysml2CapellaUtils.trace(this, eResource, objecFlow, port, "NewPort");
-			return port;
+			return manageExchangeWithCallBehavior(eResource, source, isSource, objecFlow);
 
 		}
 
 		return null;
+	}
+
+	/**
+	 * Manage the {@link ObjectFlow} port is a {@link ActivityParameterNode}
+	 * 
+	 * @param eResource
+	 *            the Sysml resource
+	 * @param source
+	 *            the {@link ActivityParameterNode}
+	 * @param mapPinToParam
+	 *            {@link Map} allowing to have the link between the {@link Pin}
+	 *            and the {@link ActivityParameterNode}
+	 * @param transformParam
+	 * @return {@link FunctionPort}
+	 */
+	private FunctionPort manageExchangeWithParam(Resource eResource, ActivityNode source,
+			Map<Pin, ActivityParameterNode> mapPinToParam, boolean transformParam) {
+		EObject sourcePortParent = source.eContainer();
+		if (sourcePortParent instanceof Activity
+				&& ((Activity) sourcePortParent).getName().equals("02 Functional Architecture")) {
+			AbstractMapping rule = MappingRulesManager.getRule(FunctionalArchitectureMapping.class.getName());
+			return (FunctionPort) rule.getMapSourceToTarget().get(source);
+		}
+		if (transformParam) {
+			ActivityParameterNode an = (ActivityParameterNode) source;
+			List<ActivityEdge> e = new ArrayList<ActivityEdge>();
+			e.addAll(an.getIncomings());
+			e.addAll(an.getOutgoings());
+			for (ActivityEdge activityEdge : e) {
+				if (activityEdge instanceof ObjectFlow) {
+					ActivityNode s = activityEdge.getSource();
+					ActivityNode t = activityEdge.getTarget();
+					boolean inDir = an.getParameter().getDirection() == ParameterDirectionKind.IN_LITERAL;
+					if (s.equals(an)) {
+
+						return getCapellaFunctionPort(eResource, t, mapPinToParam, inDir, true,
+								(ObjectFlow) activityEdge);
+					}
+					if (t.equals(an)) {
+						return getCapellaFunctionPort(eResource, s, mapPinToParam, inDir, true,
+								(ObjectFlow) activityEdge);
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Manage the {@link ObjectFlow} port is a {@link Pin}
+	 * 
+	 * @param eResource
+	 *            the Sysml resource
+	 * @param source
+	 *            the {@link Pin} source
+	 * @param mapPinToParam
+	 *            {@link Map} allowing to have the link between the {@link Pin}
+	 *            and the {@link ActivityParameterNode}
+	 * @param objecFlow
+	 *            the source exchange
+	 * @return {@link FunctionPort}
+	 */
+	private FunctionPort manageExchangeWithPin(Resource eResource, ActivityNode source,
+			Map<Pin, ActivityParameterNode> mapPinToParam, ObjectFlow objecFlow) {
+		ActivityParameterNode parameter = mapPinToParam.get(source);
+		if (parameter != null) {
+			return getCapellaFunctionPort(eResource, parameter, mapPinToParam, source instanceof InputPin, true,
+					objecFlow);
+		}
+		EObject sourcePortParent = source.eContainer();
+		AbstractMapping rule = MappingRulesManager.getRule(LogicalFunctionPortMapping.class.getName()
+				+ Sysml2CapellaUtils.getSysMLID(eResource, sourcePortParent));
+		if (rule == null) {
+			return null;
+		}
+		return (FunctionPort) rule.getMapSourceToTarget().get(source);
+	}
+
+	/**
+	 * Manage the {@link ObjectFlow} has not port.
+	 * 
+	 * @param eResource
+	 *            the Sysml resource
+	 * @param source
+	 *            the {@link CallBehaviorAction} source
+	 * @param isSource
+	 *            the port is source
+	 * @param objecFlow
+	 *            the exchange
+	 * @return {@link FunctionPort}
+	 */
+	private FunctionPort manageExchangeWithCallBehavior(Resource eResource, ActivityNode source, boolean isSource,
+			ObjectFlow objecFlow) {
+		Object object = MappingRulesManager.getRule(LogicalFunctionMapping.class.getName()).getMapSourceToTarget()
+				.get(source);
+		LogicalFunction lfParent = null;
+		if (object instanceof LogicalFunction) {
+			lfParent = (LogicalFunction) object;
+		} else if (object instanceof List) {
+			List<?> list = (List<?>) object;
+			for (Object object2 : list) {
+				if (object2 instanceof LogicalFunction) {
+					lfParent = (LogicalFunction) object2;
+					break;
+				}
+			}
+		}
+
+		FunctionPort port = null;
+		if (isSource) {
+			port = FaFactory.eINSTANCE.createFunctionInputPort();
+			lfParent.getInputs().add((FunctionInputPort) port);
+
+		} else {
+			port = FaFactory.eINSTANCE.createFunctionOutputPort();
+			lfParent.getOutputs().add((FunctionOutputPort) port);
+		}
+		port.setName(objecFlow.getName());
+		Sysml2CapellaUtils.trace(this, eResource, objecFlow, port, "NewPort");
+		return port;
 	}
 
 }
