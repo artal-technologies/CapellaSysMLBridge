b6fd5a710af08ede0649f79fa0910bbd529db1fd
BINOT Yann <yann.binot@artal.fr>
2019-06-14 16:50:46 +0200

    CSBY-18: manage connection between CR and manage connection with Actor

--- Plugins/com.artal.capella.mapping.sysml2capella/src/com/artal/capella/mapping/capella2sysml/rules/CapabilitiesRealizationsMapping.java
+++ Plugins/com.artal.capella.mapping.sysml2capella/src/com/artal/capella/mapping/capella2sysml/rules/CapabilitiesRealizationsMapping.java
@@ -9,15 +9,25 @@
  *******************************************************************************/
 package com.artal.capella.mapping.capella2sysml.rules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.diffmerge.bridge.mapping.api.IMappingExecution;
+import org.eclipse.uml2.uml.Actor;
+import org.eclipse.uml2.uml.Association;
+import org.eclipse.uml2.uml.Include;
 import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.Property;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UseCase;
-import org.polarsys.capella.core.data.capellacommon.AbstractCapabilityPkg;
+import org.polarsys.capella.core.data.capellacore.InvolvedElement;
 import org.polarsys.capella.core.data.capellamodeller.Project;
+import org.polarsys.capella.core.data.cs.ActorCapabilityRealizationInvolvement;
+import org.polarsys.capella.core.data.interaction.AbstractCapabilityInclude;
 import org.polarsys.capella.core.data.la.CapabilityRealization;
 import org.polarsys.capella.core.data.la.CapabilityRealizationPkg;
+import org.polarsys.capella.core.data.la.LogicalActor;
 
 import com.artal.capella.mapping.CapellaBridgeAlgo;
 import com.artal.capella.mapping.rules.AbstractMapping;
@@ -54,10 +64,55 @@ public class CapabilitiesRealizationsMapping extends AbstractMapping {
 				.getOwnedCapabilityRealizations();
 		Package useCasePkg = (Package) MappingRulesManager.getCapellaObjectFromAllRules(_source + "USECASES");
 
+		List<AbstractCapabilityInclude> includes = new ArrayList<>();
+		List<ActorCapabilityRealizationInvolvement> acris = new ArrayList<>();
 		for (CapabilityRealization capabilityRealization : ownedCapabilityRealizations) {
+			includes.addAll(capabilityRealization.getIncludes());
+			acris.addAll(capabilityRealization.getOwnedActorCapabilityRealizations());
+
 			transformCapabilityRealization(capabilityRealization, useCasePkg);
 		}
+		// includes
+		for (AbstractCapabilityInclude include : includes) {
+			transformIncludes(include);
+		}
 
+		// actor Capability Realization involvements
+		for (ActorCapabilityRealizationInvolvement acri : acris) {
+			CapabilityRealization cr = (CapabilityRealization) acri.eContainer();
+			InvolvedElement involved = acri.getInvolved();
+
+			if (involved instanceof LogicalActor) {
+				UseCase usecase = (UseCase) MappingRulesManager.getCapellaObjectFromAllRules(cr);
+				Actor umlActor = (Actor) MappingRulesManager.getCapellaObjectFromAllRules(involved);
+
+				Association association = UMLFactory.eINSTANCE.createAssociation();
+				useCasePkg.getPackagedElements().add(association);
+				Sysml2CapellaUtils.trace(this, _source.eResource(), acri, association, "_ASSOCIATION");
+
+				Property propertySource = association.createOwnedEnd("", usecase);
+				Property propertyTarget = association.createOwnedEnd("", umlActor);
+
+				Sysml2CapellaUtils.trace(this, _source.eResource(), acri + "SOURCE", propertySource, "SOURCE_END_");
+				Sysml2CapellaUtils.trace(this, _source.eResource(), acri + "TARGET", propertyTarget, "TARGET_END_");
+
+			}
+		}
+
+	}
+
+	/**
+	 * @param include
+	 */
+	private void transformIncludes(AbstractCapabilityInclude include) {
+		CapabilityRealization cr = (CapabilityRealization) include.eContainer();
+		CapabilityRealization addition = (CapabilityRealization) include.getIncluded();
+
+		UseCase object = (UseCase) getMapSourceToTarget().get(cr);
+		UseCase add = (UseCase) getMapSourceToTarget().get(addition);
+
+		Include umlInclude = object.createInclude("", add);
+		Sysml2CapellaUtils.trace(this, _source.eResource(), include, umlInclude, "INCLUDE_");
 	}
 
 	private void transformCapabilityRealization(CapabilityRealization capabilityRealization, Package useCasePkg) {
